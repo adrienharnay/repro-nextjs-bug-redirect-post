bd165d7bac7cf623df61dac9dfb3d921026e3675
Adrien HARNAY <adrien@harnay.me>
2024-06-18 23:29:45 +0200

    tests

--- repo-to-proxy/src/pages/test.tsx
+++ repo-to-proxy/src/pages/test.tsx
@@ -1,9 +1,31 @@
-const Test = () => {
+import { useState } from "react";
+
+const Home = () => {
+  const [data, setData] = useState(null);
+
   return (
     <div>
-      <a href="/">Go back to home</a>
+      <a href="/home">Go back home</a>
+
+      <button
+        onClick={async () => {
+          try {
+            const res = await fetch("http://localhost:3000/api/hello", {
+              method: "POST",
+              mode: "no-cors",
+            });
+            const newData = await res.json();
+            setData(newData);
+          } catch (error) {
+            console.error(error);
+          }
+        }}
+      >
+        Trigger POST
+      </button>
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </div>
   );
 };
 
-export default Test;
+export default Home;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+const Test = () => {
+  return (
+    <div>
+      <a href="/test">Go to test page</a>
+    </div>
+  );
+};
+
+export default Test;
